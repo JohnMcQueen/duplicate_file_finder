3f04f3abd73ca652fa7f71509553dbcbe161f40a
John McQueen <jmcqueen144@gmail.com>
2025-12-28 13:40:03 -0600

    added file hashing to match for exact duplicates

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,20 @@
+use sha2::{Digest, Sha256};
 use std::collections::HashMap;
+use std::fs::File;
+use std::io;
 use walkdir::WalkDir;
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct FileMap {
     // keys will be file sizes and value will be vectors of file paths
     pub files: HashMap<String, Vec<String>>,
 }
 
 impl FileMap {
-    pub fn new() -> FileMap {
-        FileMap {
-            files: HashMap::new(),
-        }
-    }
-
     pub fn add_file(&mut self, file_size: u64, file_name: String) {
         self.files
             .entry(file_size.to_string())
-            .or_insert(Vec::new())
+            .or_default()
             .push(file_name);
     }
 
@@ -31,10 +28,48 @@ impl FileMap {
             }
         }
     }
+
+    pub fn find_true_duplicates(&self) -> Result<(), Box<dyn std::error::Error>> {
+        for (_size, files) in &self.files {
+            // Only hash if there are duplicates potentially
+            if files.len() > 1 {
+                let mut hashed_files: HashMap<String, Vec<String>> = HashMap::new();
+
+                for file_path in files {
+                    let hash = hash_files(file_path)?;
+                    hashed_files
+                        .entry(hash)
+                        .or_default()
+                        .push(file_path.clone());
+                }
+
+                // TODO: printing for now but in future should do something with the duplicates
+                for (hash, paths) in &hashed_files {
+                    if paths.len() > 1 {
+                        println!(" Duplicates found (hash: {})", hash);
+                        for path in paths {
+                            println!("    - {}", path);
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
-pub fn gather_files(directory: &String) -> Result<(), Box<dyn std::error::Error>> {
-    // let mut file_paths = Vec::new();
-    let mut file_map = FileMap::new();
+
+pub fn check_for_duplicates(directory: &String) {
+    let file_map = gather_files(directory);
+
+    match file_map {
+        Ok(v) => println!("result {:#?}", v),
+        Err(e) => panic!("error!: {e}"),
+    }
+}
+
+pub fn gather_files(directory: &String) -> Result<FileMap, Box<dyn std::error::Error>> {
+    let mut file_map = FileMap::default();
 
     for entry in WalkDir::new(directory) {
         let entry = entry?;
@@ -44,13 +79,19 @@ pub fn gather_files(directory: &String) -> Result<(), Box<dyn std::error::Error>
             let file_size = metadata.len();
             let file_path = entry.path().to_path_buf();
 
-            println!("{} - {} bytes", file_path.display(), file_size);
+            println!("{}", file_path.display());
 
             file_map.add_file(file_size, file_path.display().to_string());
         }
     }
 
-    println!("{:#?}", file_map);
+    Ok(file_map)
+}
 
-    Ok(())
+fn hash_files(path: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let mut file = File::open(path)?;
+    let mut hasher = Sha256::new();
+    io::copy(&mut file, &mut hasher)?;
+
+    Ok(format!("{:x}", hasher.finalize()))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-use duplicate_finder::FileMap;
 use duplicate_finder::gather_files;
 use std::env;
 
 fn main() {
-    println!("Hello, world!");
-
     // collecting user input directory to scan
     let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
-    println!("{query}");
+    println!("User inputed directory to scan: {query}");
 
-    let _files = gather_files(query);
+    match gather_files(query) {
+        Ok(file_map) => {
+            file_map.find_true_duplicates().unwrap();
+        }
+        Err(e) => {
+            eprintln!("Error gathering files: {}", e);
+        }
+    }
 }
