ca42b1fd5d5558468e3a4fb2cc0a2395d1617f3e
John McQueen <jmcqueen144@gmail.com>
2025-12-29 19:12:56 -0600

    adding unit tests for FileMap struct

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,16 @@ impl FileMap {
             .push(file_name);
     }
 
+    pub fn get_duplicates(&self) -> HashMap<String, Vec<String>> {
+        self.files
+            .iter()
+            .filter(|(_, files)| files.len() > 1)
+            .map(|(size, files)| (size.clone(), files.clone()))
+            .collect()
+    }
+
     pub fn print_duplicates(&self) {
-        for (size, files) in &self.files {
+        for (size, files) in &self.get_duplicates() {
             if files.len() > 1 {
                 println!("\nFiles with size {} bytes:", size);
                 for file in files {
@@ -30,7 +38,7 @@ impl FileMap {
     }
 
     pub fn find_true_duplicates(&self) -> Result<(), Box<dyn std::error::Error>> {
-        for (_size, files) in &self.files {
+        for (_, files) in &self.files {
             // Only hash if there are duplicates potentially
             if files.len() > 1 {
                 let mut hashed_files: HashMap<String, Vec<String>> = HashMap::new();
@@ -94,4 +102,44 @@ fn hash_files(path: &str) -> Result<String, Box<dyn std::error::Error>> {
     io::copy(&mut file, &mut hasher)?;
 
     Ok(format!("{:x}", hasher.finalize()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_default_file_map() {
+        let file_map = FileMap::default();
+
+        assert_eq!(file_map.files.len(), 0);
+    }
+
+    #[test]
+    fn test_adding_file() {
+        let file_name = "test".to_string();
+        let file_length: u64 = 883223;
+        let mut file_map = FileMap::default();
+
+        file_map.add_file(file_length, file_name);
+
+        assert_eq!(
+            file_map.files.get("883223").unwrap(),
+            &vec!["test".to_string()]
+        );
+    }
+
+    #[test]
+    fn test_get_duplicates() {
+        let mut file_map = FileMap::default();
+        file_map.add_file(100, "file1.txt".to_string());
+        file_map.add_file(100, "file2.txt".to_string());
+        file_map.add_file(200, "file3.txt".to_string());
+
+        let duplicates = file_map.get_duplicates();
+
+        assert_eq!(duplicates.len(), 1); // Only one size group has duplicates
+        assert!(duplicates.contains_key("100"));
+        assert_eq!(duplicates.get("100").unwrap().len(), 2);
+    }
 }
