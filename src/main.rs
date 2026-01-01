73c599f802d01215818523546d425535505a44b8
John McQueen <jmcqueen144@gmail.com>
2026-01-01 15:56:23 -0600

    add some error handling if arguments aren't passed

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ fn main() {
     // collecting user input directory to scan
     let args: Vec<String> = env::args().collect();
 
+    if args.len() < 2 {
+        eprintln!("Error: Missing directory argument");
+        eprintln!("Usage: {} <directory>", args[0]);
+        std::process::exit(1);
+    }
+
     let query = &args[1];
     println!("User inputed directory to scan: {query}");
 
